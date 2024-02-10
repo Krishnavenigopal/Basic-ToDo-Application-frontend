5a016441b55149617456e105f20b4a14a3a06196
krishnaveni <krishnaveniv90@gmail.com>
2024-02-10 01:11:09 +0100

    front end changes

---
+++ frontend/src/utils/service.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const baseUrl = "http://localhost:5000"
+
+const getAllToDo = (setToDo) => {
+    axios.get(baseUrl)
+    .then(({data}) => {
+        console.log('data --->', data);
+        setToDo(data)
+    })
+}
+
+const addToDo = (text, setText, setToDo) => {
+    axios.post(`${baseUrl}/save`, {text})
+    .then((data) => {
+        console.log(data);
+        setText("")
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const updateToDo = (toDoId, text, setToDo, setText, setUpdating) => {
+    axios.post(`${baseUrl}/update`, {_id: toDoId, text})
+    .then((data) => {
+        setText("")
+        setUpdating(false);
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+const deleteToDo = (_id, setToDo) => {
+    axios.post(`${baseUrl}/delete`, {_id})
+    .then((data) => {
+        console.log("delete",data)
+        getAllToDo(setToDo)
+    })
+    .catch((err) => console.log(err))
+}
+
+export {getAllToDo, addToDo, updateToDo, deleteToDo}
